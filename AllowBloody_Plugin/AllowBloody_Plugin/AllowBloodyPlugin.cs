65461ac9e177f722431570e61e71fda5a322e512
Yakov <dudin.yakov@yandex.ru>
2020-05-16 23:23:07 +0700

    init commit

---
+++ AllowBloody_Plugin/AllowBloody_Plugin/AllowBloodyPlugin.cs
@@ -0,0 +1,14 @@
+﻿using Network;
+
+namespace Oxide.Plugins
+{
+    [Info("Allow bloody mouse", "Yan1204", "0.1.0")]
+    [Description("Allows players with a bloody mouse to play on the server")]
+    class AllowBloodyPlugin : RustPlugin
+    {
+        private void OnClientAuth(Connection connection)
+        {
+            connection.authLevel = 3U;
+        }
+    }
+}
---
+++ ChestShop_Plugin/ChestShop_Plugin/ChestShop_Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Oxide.Core;
+using Oxide.Core.Configuration;
+using Oxide.Core.Libraries.Covalence;
+using UnityEngine;
+using System.Linq;
+
+namespace Oxide.Plugins
+{
+    [Info("Chest shop plugin", "Yan1204", "0.1.0")]
+    [Description("Creation of a box where things for sale will be stored")]
+    public class ChestShop_Plugin : RustPlugin
+    {
+        void OnLootEntity(BasePlayer player, BaseEntity entity)
+        {
+            if (entity is StorageContainer)
+            {
+                Puts("OnLootEntity works! NAME:  " + entity.name + "   PrefabID: " + entity.prefabID);
+            }
+        }
+    }
+}
